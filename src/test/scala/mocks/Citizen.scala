db320eb54e67475db55370058f32ab597688b4c6
Peerapat A <nuboat@gmail.com>
2018-04-25 11:11:21 +0700

    Update Mock data

--- src/test/scala/mocks/Citizen.scala
+++ src/test/scala/mocks/Citizen.scala
@@ -1,6 +1,9 @@
 package mocks
 
+import in.norbor.yoda.annotations.TableSchema
+
 /**
   * Created by Peerapat A on Mar 22, 2017
   */
+@TableSchema(name = "citizens", pk = "citizen_id")
 case class Citizen(citizenId: Long)
